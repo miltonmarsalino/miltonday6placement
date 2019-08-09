dcd04ba5c946be9cfe8664f795fbfa647e449184
miltonmarsalino <46523397+miltonmarsalino@users.noreply.github.com>
2019-08-09 12:26:06 +0530

    Create 1.6helloworld.py

---
+++ 1.6helloworld.py
@@ -0,0 +1,7 @@
+n= str(input())
+for index in range (0,len(n)):
+	if index%2 == 0:
+		print(n[index] , end = '')n= str(input())
+for index in range (0,len(n)):
+	if index%2 == 0:
+		print(n[index] , end = '')
