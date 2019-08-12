beb8240d888bb44f75a8bd168963e44e43f50b1b
miltonmarsalino <46523397+miltonmarsalino@users.noreply.github.com>
2019-08-12 11:39:39 +0530

    Create avgtol.py

---
+++ avgtol.py
@@ -0,0 +1,21 @@
+# your code goes here
+import array as eee
+total = 0
+a = eee.array('i',[30,90,50,60,100])
+for i in range (0,len(a)):
+    total = total +a[i]
+    
+average = total/len(a)
+print(total)
+print(average)
+  # your code goes here
+import array as eee
+total = 0
+a = eee.array('i',[30,90,50,60,100])
+for i in range (0,len(a)):
+    total = total +a[i]
+    
+average = total/len(a)
+print(total)
+print(average)
+  
